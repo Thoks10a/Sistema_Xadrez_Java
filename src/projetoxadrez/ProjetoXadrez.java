324b016746f667bec547485a58f1067b2ddb6318
Nicholas Isola <nicholas.isola@hotmail.com>
2020-05-22 15:30:07 -0300

    First class: Position

--- src/projetoxadrez/ProjetoXadrez.java
+++ src/projetoxadrez/ProjetoXadrez.java
@@ -1,14 +1,16 @@
 
 package projetoxadrez;
 
+import BoardGame.Position;
+
 
 public class ProjetoXadrez {
 
 
     public static void main(String[] args) {
         
-        System.out.println("Hello world");
-        
+        Position pos = new Position(3,5);
+        System.out.println(pos);
     }
     
 }
