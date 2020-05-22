4aa4c2a0462051325162f53b550ebefcdb0cd928
Nicholas Isola <nicholas.isola@hotmail.com>
2020-05-22 15:22:19 -0300

    Project created

---
+++ src/projetoxadrez/ProjetoXadrez.java
@@ -0,0 +1,14 @@
+
+package projetoxadrez;
+
+
+public class ProjetoXadrez {
+
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello world");
+        
+    }
+    
+}
