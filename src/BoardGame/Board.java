632f8e160ed94591d85fd87804b76947a9e11b28
Nicholas Isola <nicholas.isola@hotmail.com>
2020-06-02 14:38:31 -0300

    Moving Pieces

--- src/BoardGame/Board.java
+++ src/BoardGame/Board.java
@@ -25,14 +25,14 @@ public class Board {
     }
     
     public Piece piece(int row,int column){
-        if(!postionExists (row,column)){
+        if(!positionExists (row,column)){
             throw new BoardException("Postion not on the board");
         }
         return pieces[row][column];
     }
     
         public Piece piece(Position position){
-             if(!postionExists (position)){
+             if(!positionExists (position)){
             throw new BoardException("Postion not on the board");
         }
         return pieces [position.getRow()][position.getColumn()];
@@ -44,14 +44,26 @@ public class Board {
             pieces[position.getRow()][position.getColumn()] = piece;
             piece.position = position;
         }
-        private boolean postionExists(int row, int column){
+        public Piece removePiece(Position position){
+            if(!positionExists (position)){
+                throw new BoardException("Position not on the board");
+            }
+            if(piece(position) == null){
+                return null;
+            }
+            Piece aux = piece(position);
+            aux.position = null;
+            pieces[position.getRow()][position.getColumn()] = null;
+            return aux;
+        }
+        private boolean positionExists(int row, int column){
            return row >= 0 && row < rows && column >= 0 && column < columns;
         }
-        public boolean postionExists(Position position){
-            return postionExists(position.getRow(),position.getColumn());
+        public boolean positionExists(Position position){
+            return positionExists(position.getRow(),position.getColumn());
         }
         public boolean thereIsAPiece(Position position){
-             if(!postionExists (position)){
+             if(!positionExists (position)){
             throw new BoardException("Postion not on the board");
         }
            return piece(position) != null;
--- src/application/ProjetoXadrez.java
+++ src/application/ProjetoXadrez.java
@@ -3,6 +3,9 @@ package application;
 
 import BoardGame.Board;
 import ProjetoXadrez.ChessMatch;
+import ProjetoXadrez.ChessPiece;
+import ProjetoXadrez.ChessPosition;
+import java.util.Scanner;
 
 
 
@@ -10,9 +13,20 @@ public class ProjetoXadrez {
 
 
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
        ChessMatch chessMatch = new ChessMatch();
-       UI.printBoard(chessMatch.getPieces());
+       
+       while(true){
+           UI.printBoard(chessMatch.getPieces());
+           System.out.println();
+           System.out.println("Source: ");
+           ChessPosition source = UI.readChessPosition(sc);
+           System.out.println();
+           System.out.println("Target: ");
+           ChessPosition target = UI.readChessPosition(sc);
+           ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+       }
+       }
     }
     
-}
+
